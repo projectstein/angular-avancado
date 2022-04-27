010fe060f7ec7a6443f00bba20337494f17d5993
Felipe Stein Rosa <projectstein@gmail.com>
2022-04-27 14:00:45 -0300

    chore: add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
 import { InMemoryDatabase } from './in-memory-database';
 import { NgModule } from '@angular/core';
 
@@ -12,6 +13,7 @@ import { NgModule } from '@angular/core';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
